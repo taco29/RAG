fc2040ce541a1141ad407b3c93708d48571b17c3
Vũ Huy <huyvu292007@gmail.com>
2026-01-29 00:35:49 +0700

    OOP

--- embedding.py
+++ embedding.py
@@ -1,6 +1,17 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
-def get_embedding():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+class EmbeddingModel:
+    def __init__(
+        self, 
+        model_name: str = "sentence-transformers/all-MiniLM-L6-v2",
+        nomalize: bool = True
+    ):
+        self.model_name = model_name
+        self.nomalize = nomalize
+        self.embedding = HuggingFaceEmbeddings(
+            model_name=self.model_name,
+            encode_kwargs={"normalize_embeddings": self.nomalize}
+        )
+    def get(self):
+        return self._embedding
+    
