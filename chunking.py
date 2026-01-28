9a52251e1898cf5df0db97ffb589404947135e34
Vũ Huy <huyvu292007@gmail.com>
2026-01-29 00:36:23 +0700

    OOP...

--- chunking.py
+++ chunking.py
@@ -2,28 +2,27 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
-def load_text(path):
-    loader = TextLoader(path, encoding="utf-8")
-    docs = loader.load()
-    return docs[0].page_content
+DEFAULT_SEPARATORS = ["\n\n", "\n", "▪", "•", "◦", " ", ""]
 
-def chunking(documents, chunk_size, chunk_overlap):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        separators=[
-            "\n\n",
-            "\n",
-            "▪",
-            "•",
-            "◦",
-            " ",
-            ""
-        ]
-    )
-    chunks = text_splitter.split_text(documents)
-    return chunks
+class TextChunker:
+    def __init__(self, chunk_size, chunk_overleap):
+        self.chunk_size = chunk_size
+        self.chunk_overleap = chunk_overleap
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size= self.chunk_size,
+            chunk_overleap=self.chunk_overleap,
+            separators= DEFAULT_SEPARATORS                                                          
+        )
+    def load_and_chunk(self, path: str):
+        loader = TextLoader(path, encoding="utf-8")
+        text = loader.load()[0].page_content
+        chunks = self.splitter.split_text(text)
+        return [Document(page_content=c) for c in chunks]
 
-def create_documents(chunks):
-    docs = [Document(page_content=chunk) for chunk in chunks]
-    return docs
+
+class create_documents:
+    def __init__(self, chunks):
+        self.chunks= chunks
+
+    def create_docs(self):
+        docs = Document(page_content=self.chunks)
