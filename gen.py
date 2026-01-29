c63531ab97f6f44b9908ba91d90cefd2bd03fc47
Vũ Huy <huyvu292007@gmail.com>
2026-01-29 14:12:43 +0700

    OOP

--- gen.py
+++ gen.py
@@ -2,15 +2,8 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_classic.chains.retrieval_qa.base import RetrievalQA
-from retrieve import Retriever
-from langchain_core.prompts import ChatPromptTemplate
 
-API_KEY = ""
-os.environ["GOOGLE_API_KEY"] = API_KEY
-
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
-
-prompt = ChatPromptTemplate.from_template("""
+PROMT = """
 Bạn là một trợ giảng Machine Learning.
 
 Nhiệm vụ của bạn là trả lời câu hỏi CHỈ dựa trên thông tin có trong CONTEXT.
@@ -38,34 +31,25 @@ Yêu cầu câu trả lời:
 - Không thêm ví dụ ngoài tài liệu
 
 CÂU TRẢ LỜI:
-""")
+"""
 
-query = "Markov Networks là gì? Chúng được ứng dụng như thế nào trong Machine Learning?"
+class AnswerGenerator:
+    def __init__(self, model_name: str = "gemini-2.5-flash", temperature: float = 0.0):
+        self.llm = ChatGoogleGenerativeAI(model=model_name, temperature=temperature)
+        self.prompt = self._build_prompt()
 
-path = r"C:\Users\Admin\Code\Chatbot\RAG\data.txt"
-retriever_instance = Retriever(path, k=3).get_retriever()
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm=model,
-    chain_type="stuff",
-    retriever=retriever_instance,
-    return_source_documents=True,
-    chain_type_kwargs={"prompt": prompt}
-)
-result = qa_chain({"query": query})
-
-print("ANSWER:")
-print(result["result"])
-
-print("\nSOURCES:")
-for doc in result["source_documents"]:
-    print("-", doc.metadata)
-
-
-
-print("\n--- CHUNKS IN THE DOCUMENT ---")
-docs = Retriever(path, k=3).get_chunks()
-for i, doc in enumerate(docs):
-    print(f"Chunk {i+1}:")
-    print(doc.page_content)
-    print("--------------------")   
+    def _build_prompt(self):
+        return ChatPromptTemplate.from_template(PROMT)
+    
+    def create_chain(self, retriever):
+        return RetrievalQA.from_chain_type(
+            llm=self.llm,
+            chain_type="stuff",
+            retriever=retriever,
+            return_source_documents=True,
+            #chain_type_kwargs={"prompt": self.prompt}
+        )
+    
+    def answer(self, retriever, question: str):
+        qa_chain = self.create_chain(retriever)
+        return qa_chain({"query": question})
