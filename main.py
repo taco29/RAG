985dd9840674536e1ecf3ba90a5c3d7365f6b33a
Vũ Huy <huyvu292007@gmail.com>
2026-01-29 14:13:30 +0700

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from retrieve import Retriever
+from gen import AnswerGenerator
+
+API_KEY = "AIzaSyDy_uT7SS9X1uA6SRiNE04s9zk3wQFfTww"
+os.environ["GOOGLE_API_KEY"] = API_KEY
+
+def main():
+    path = r"C:\Users\Admin\Code\Chatbot\RAG\data.txt"
+    question = "Bagging là gì?"
+
+    retriever = Retriever(path, k=3).get_retriever()
+    result = AnswerGenerator().answer(retriever, question)
+    print("ANSWER:")
+    print(result["result"])
+
+    print("\nSOURCES:")
+    for doc in result["source_documents"]:
+        print("-", doc.metadata)
+        print(doc.page_content[:200], "...\n")
+
+    print("\n--- CHUNKS IN THE DOCUMENT ---")
+    docs = Retriever(path, k=3).get_chunks()
+    for i, doc in enumerate(docs):
+        print(f"Chunk {i+1}:")
+        print(doc.page_content)
+    print("--------------------")   
+if __name__ == "__main__":
+    main()
